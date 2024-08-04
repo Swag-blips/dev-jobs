1170d1b83f970220bc0d4d8a82035d87588db0f5
Swag-blips <coderblip@gmail.com>
2024-08-04 16:56:05 +0100

    page restructure, react router dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from "react";
-import Header from "./components/Header";
-import Jobs from "./components/Jobs";
+import HomePage from "./pages/HomePage";
+import ErrorPage from "./pages/ErrorPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import JobPage from "./pages/JobPage";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/jobs/:id",
+      element: <JobPage />,
+      errorElement: <ErrorPage />,
+    },
+  ]);
+
   return (
-    <div className="font-kubmh">
-      <Header />
-      <Jobs />
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import Jobs from "../components/Jobs";
+
+const HomePage = () => {
+  return (
+    <div className="font-kubmh">
+      <Header />
+      <Jobs />
+    </div>
+  );
+};
+
+export default HomePage;
